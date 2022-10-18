e3f4148032d9a44767f78dddda50786c056d4601
Jes-Curious <64395676+Jes-Curius@users.noreply.github.com>
2022-10-18 21:22:01 +1300

    Added dependencies

---
+++ config-overrides.js
@@ -0,0 +1,21 @@
+/* config-overrides.js */
+
+module.exports = {
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.less$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' },
+                    { loader: 'less-loader' }
+                ]
+            }
+        ]
+    },
+}
